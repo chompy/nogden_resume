f3c750c853c123e753fea60c76057027898c9db6
Nathan Ogden <nathan@ogden.tech>
2024-09-12 16:28:34 -0400

    added condensed version of resume and updated code to compile multiple resume types

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# output as html (base)
-go run . data/main.yaml > resume.html
+for path_to in data/*.yaml
+do
+    filename="$(basename $path_to)"
+    name="${filename%.*}"
+    # output as html (base)
+    go run . data/$filename > out/resume_$name.html
+    # output as pdf via headless Chromium
+    chromium --headless --disable-gpu --run-all-compositor-stages-before-draw --no-pdf-header-footer --print-to-pdf="out/resume_$name.pdf" out/resume_$name.html
+done
 
-# output as pdf via headless Chromium
-chromium --headless --disable-gpu --run-all-compositor-stages-before-draw --no-pdf-header-footer --print-to-pdf="resume.pdf" resume.html
+cp out/resume_main.pdf out/resume.pdf
